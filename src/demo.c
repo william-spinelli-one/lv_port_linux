12d51ad0cf76b49bf1a974aae30e5476da255d5a
William Spinelli <174336620+william-spinelli-one@users.noreply.github.com>
2025-09-30 16:54:17 +0200

    Add animation

--- src/demo.c
+++ src/demo.c
@@ -8,6 +8,14 @@
 #define LVGL_RESOURCES_PATH "A:/projects/lvgl/lv_port_linux/src/resources/"
 #define RESOURCES_PATH "/projects/lvgl/lv_port_linux/src/resources/"
 
+static void scroll_x_anim_cb(void *obj, int32_t v)
+{
+    lv_obj_scroll_to_x(obj, v, LV_ANIM_OFF);
+}
+
+static bool animation_running = false;
+static lv_obj_t *animation_container = NULL;
+
 void demo_widgets(void)
 {
     lv_demo_widgets();
@@ -18,23 +26,54 @@ void demo_widgets(void)
 #define PNG_IMAGES 3
 #define JPG_IMAGES 2
 
+static void toggle_scroll_animation_event_cb(lv_event_t * e)
+{
+    lv_obj_t *btn = lv_event_get_target(e);
+    lv_obj_t *container = lv_event_get_user_data(e);
+    lv_obj_t *btn_label = lv_obj_get_child(btn, 0);
+
+    if (animation_running) {
+        lv_anim_delete(container, scroll_x_anim_cb);
+        animation_running = false;
+        lv_label_set_text(btn_label, "SCROLL ON");
+    } else {
+        int32_t content_width = lv_obj_get_scroll_right(container);
+
+        lv_anim_t a;
+        lv_anim_init(&a);
+        lv_anim_set_var(&a, container);
+        lv_anim_set_exec_cb(&a, scroll_x_anim_cb);
+        lv_anim_set_values(&a, 0, content_width);
+        lv_anim_set_duration(&a, 20000);
+        lv_anim_set_playback_duration(&a, 20000);
+        lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+        lv_anim_set_path_cb(&a, lv_anim_path_linear);
+
+        lv_anim_start(&a);
+        animation_running = true;
+        animation_container = container;
+        lv_label_set_text(btn_label, "SCROLL OFF");
+    }
+}
+
 void demo_scroll(void)
 {
-    lv_obj_t *screen = lv_screen_active();
-
     lv_font_t *roboto_font = lv_freetype_font_create(RESOURCES_PATH "Roboto-Bold.ttf", LV_FREETYPE_FONT_RENDER_MODE_BITMAP, 96, LV_FREETYPE_FONT_STYLE_NORMAL);
     if (!roboto_font) {
         printf("Failed to load Roboto font!\n");
         return;
     }
 
+    lv_obj_t *screen = lv_screen_active();
+
+
     lv_obj_t *container = lv_obj_create(screen);
-    lv_obj_set_size(container, LV_HOR_RES, LV_VER_RES);
+    lv_obj_set_size(container, LV_HOR_RES, LV_VER_RES - 100);
     lv_obj_set_flex_flow(container, LV_FLEX_FLOW_ROW);
     lv_obj_set_scrollbar_mode(container, LV_SCROLLBAR_MODE_ON);
     lv_obj_set_scroll_dir(container, LV_DIR_HOR);
-    lv_obj_set_scroll_snap_x(container, LV_SCROLL_SNAP_CENTER);
-    lv_obj_center(container);
+    lv_obj_set_scroll_snap_x(container, LV_SCROLL_SNAP_NONE);
+    lv_obj_align(container, LV_ALIGN_TOP_MID, 0, 0);
 
     lv_obj_set_style_pad_all(container, 0, 0);
     lv_obj_set_style_pad_column(container, 0, 0);
@@ -61,4 +100,14 @@ void demo_scroll(void)
         lv_obj_set_style_text_color(label, lv_color_hex(0xFFFFFF), 0);
         lv_obj_center(label);
     }
+
+    lv_obj_t *btn = lv_button_create(screen);
+    lv_obj_set_size(btn, 600, 80);
+    lv_obj_align(btn, LV_ALIGN_BOTTOM_MID, 0, -10);
+    lv_obj_add_event_cb(btn, toggle_scroll_animation_event_cb, LV_EVENT_CLICKED, container);
+
+    lv_obj_t *btn_label = lv_label_create(btn);
+    lv_label_set_text(btn_label, "SCROLL ON");
+    lv_obj_set_style_text_font(btn_label, roboto_font, 0);
+    lv_obj_center(btn_label);
 }
